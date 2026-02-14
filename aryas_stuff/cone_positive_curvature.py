fe6a0534b842caadf336c88a7863ae2daf018516
Arya Yae <aryacwazy@gmail.com>
2026-02-13 18:52:34 -0800

    Cylinder simulation

--- aryas_stuff/cone_positive_curvature.py
+++ aryas_stuff/cone_positive_curvature.py
@@ -15,12 +15,12 @@ RHO = 0.5
 
 rMAX = 1.0
 
-tMAX = 5.0
+tMAX = 10.0
 
 # Get radii in [0,1]
 a=0.1
-Nr=150
-Ntheta=16
+Nr=60
+Ntheta=6
 
 rVals = np.linspace(0.0, rMAX, Nr)
 
@@ -58,24 +58,24 @@ def metric_laplacian(f,g):
 
 # First order extrapolation on the r=0 end
 def left_extrapolate(f):
-    firstRow = f[0] + dr * (f[0] - f[1])
+    firstRow = f[0] + (f[0] - f[1])
     return np.concatenate((firstRow[None,:], f), axis=0)
 
 # Calculates curvature
 def measure_curvature(g):
-    return metric_laplacian(np.log(g),g)
+    return metric_laplacian(np.log(g) / 2, g)
 
-# Integrates a function
-def integrate(f):
+# Integrates a function with respect to metric g
+def integrate(f, g):
     return np.sum(
-        np.array([f[i] * rVals[i] * dr * dtheta for i in range(0,Nr-2)])
+        np.array([g[i] * f[i] * rVals[i] * dr * dtheta for i in range(1,Nr-2)])
         )
 
 # Measures average curvature
 def average_curvature(g):
     curvature = measure_curvature(g)
     curvature = left_extrapolate(curvature[1:])
-    return integrate(curvature) / (2 * np.pi)
+    return integrate(curvature, g) / integrate(np.full((Nr-2),1), g)
 
 def sim_in_polar(t=tMAX):
     # Background scalar curvature
@@ -95,6 +95,7 @@ def sim_in_polar(t=tMAX):
     # The model: updates for each time step t
     for n in range(tVals - 1):
         KAvg = average_curvature(u[n] * uBackground)
+        #print(KAvg)
 
         # Update excludes endpoints in rVals
         u[n+1, 1:-1] = u[n, 1:-1] + dt * (metric_laplacian(u[n,:,:],uBackground) + KAvg * u[n,1:-1,:] - KBackground)
@@ -102,8 +103,10 @@ def sim_in_polar(t=tMAX):
         # Expensive...can we update in place?
         u[n+1] = set_boundary(u[n+1])
 
-        # First order extrapolation to get rVals=0 point point
-        u[n+1, 0, :] = u[n+1, 1, :] + dr * (u[n+1, 2, :] - u[n+1, 1, :])
+        # First order extrapolation to update r=0 values
+        u[n+1, 0, :] = u[n+1, 1, :] + (u[n+1, 1, :] - u[n+1, 2, :])
+
+    print(measure_curvature(u[-1] * uBackground))
 
     R, TH = np.meshgrid(rVals, theta, indexing="ij")
 
@@ -113,16 +116,13 @@ def sim_in_polar(t=tMAX):
 
 
     #Multiply uBackground by u to get the metric g_t
-    gMetric = uBackground[None,:,:] * u[:,:]
-
-    print(measure_curvature(gMetric[-1]))
+    gMetric = uBackground * u
 
     #Remove singularity by duplicating next value
-    gMetric[:,0,:] = gMetric[:,1,:]
+    gMetric[:,0] = gMetric[:,1] + (gMetric[:,1] - gMetric[:,2])
 
     # Plot the sim
-    plot_u_with_slider(u, X, Y, dt)
-    plot_u_with_slider(gMetric, X, Y, dt)
+    plot_side_by_side_with_slider(u, gMetric, X, Y, dt)
 
 
 def set_boundary(w:np.ndarray):
@@ -133,60 +133,57 @@ def set_boundary(w:np.ndarray):
     return l
 
 
-def plot_u_with_slider(u, X, Y, dt):
-    tVals = u.shape[0]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+def plot_side_by_side_with_slider(u1, u2, X, Y, dt, title1="u", title2="g"):
+    tVals = u1.shape[0]
+    fig = plt.figure(figsize=(12, 6))
+    
+    # Subplot 1
+    ax1 = fig.add_subplot(121, projection="3d")
+    zmin1 = float(u1.min())
+    zmax1 = float(u1.max())
+    ax1.set_zlim(zmin1, zmax1)
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    ax1.set_zlabel(title1)
+
+    # Subplot 2
+    ax2 = fig.add_subplot(122, projection="3d")
+    zmin2 = float(u2.min())
+    zmax2 = float(u2.max())
+    ax2.set_zlim(zmin2, zmax2)
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+    ax2.set_zlabel(title2)
+
     fig.subplots_adjust(bottom=0.18)
 
-    zmin = float(u.min())
-    zmax = float(u.max())
-    ax.set_zlim(zmin, zmax)
-
     k0 = 0
-    surf = ax.plot_surface(X, Y, u[k0], cmap="jet", vmin=zmin, vmax=zmax, shade=True)
-    ax.set_title(f"t = {k0*dt:.4f} s (k={k0})")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("Temp")
+    surf1 = ax1.plot_surface(X, Y, u1[k0], cmap="jet", vmin=zmin1, vmax=zmax1, shade=True)
+    ax1.set_title(f"{title1} at t = {k0*dt:.4f} s")
+    
+    surf2 = ax2.plot_surface(X, Y, u2[k0], cmap="jet", vmin=zmin2, vmax=zmax2, shade=True)
+    ax2.set_title(f"{title2} at t = {k0*dt:.4f} s")
 
     slider_ax = fig.add_axes([0.15, 0.06, 0.7, 0.04])
     s = Slider(slider_ax, "time index k", 0, tVals - 1, valinit=k0, valstep=1)
 
     def update(val):
-        nonlocal surf
+        nonlocal surf1, surf2
         k = int(s.val)
-        surf.remove()
-        surf = ax.plot_surface(X, Y, u[k], cmap="jet", vmin=zmin, vmax=zmax, shade=True)
-        ax.set_title(f"t = {k*dt:.4f} s (k={k})")
+        
+        surf1.remove()
+        surf1 = ax1.plot_surface(X, Y, u1[k], cmap="jet", vmin=zmin1, vmax=zmax1, shade=True)
+        ax1.set_title(f"{title1} at t = {k*dt:.4f} s")
+        
+        surf2.remove()
+        surf2 = ax2.plot_surface(X, Y, u2[k], cmap="jet", vmin=zmin2, vmax=zmax2, shade=True)
+        ax2.set_title(f"{title2} at t = {k*dt:.4f} s")
+        
         fig.canvas.draw_idle()
 
     s.on_changed(update)
     plt.show()
 
 
-
-def initialize(u, dx, dy):
-    Nt, Nx, Ny = u.shape
-
-    # Get coordinate arrays
-    x = np.arange(Nx) * dx
-    y = np.arange(Ny) * dy
-
-    # f(0, y) = 0
-    u[:, 0, :] = 0
-
-    # f(1, y) = 1-2y
-    u[:, -1, :] = 1 - (2 * y)
-
-    # f(x, 0) = sin((pi * x) /2)
-    u[:, :, 0] = np.sin(((np.pi / 2) * x))
-
-    # f(x, 1) = -sin((pi * x) /2)
-    u[:, :, -1] = -np.sin(((np.pi / 2) * x))
-
-    return u
-
-
 if __name__ == "__main__":
-    sim_in_polar(t=2)
+    sim_in_polar()
